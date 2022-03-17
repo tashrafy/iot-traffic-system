34bec0ed41af39513bd5e1729cf97fa946d07a16
Taufiqur Ashrafy <tashrafy91@gmail.com>
2022-03-16 21:06:25 -0400

    chore(submodules): switching iot-inspector-local and iot-traffic-lights to submodules

--- iot-traffic-lights/server/api/hue/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var controller = require('./hue.controller');
-
-var router = express.Router();
-
-router.get('/lights', controller.getAll);
-router.post('/modify_state', controller.modifyState);
-
-module.exports = router;
--- iot-traffic-lights/server/config/environment/shared.js
+++
@@ -1,4 +0,0 @@
-exports = module.exports = {
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin']
-};
