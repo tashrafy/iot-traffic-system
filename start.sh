7d141c4c979b84d5981559b2671bc189ff4dde1f
Taufiqur Ashrafy <tashrafy91@gmail.com>
2022-03-26 21:42:49 -0400

    fix(start): adding conditional for audio modality

--- start.sh
+++ start.sh
@@ -8,13 +8,14 @@
 export UUID=$(cat /proc/sys/kernel/random/uuid)
 echo $UUID
 docker-compose up -d &
-sleep 45
 
 # Start standalone applications:
 # 	- Iot Sound Modality
-cd iot-traffic-sound
-export ENABLE_AUDIO=true
-export WARNING_TRAFFIC_TTS="A flow of potentially concerning traffic has been identified from a device on the network."
-export TRACKING_TRAFFIC_TTS="A flow of tracking traffic has been identified from a device on the network."
-npm run start
-cd ../
+export ENABLE_AUDIO=false
+if [ true"${ENABLE_AUDIO}" == "true" ]; then 
+	sleep 45
+	cd iot-traffic-sound
+	export WARNING_TRAFFIC_TTS="A flow of potentially concerning traffic has been identified from a device on the network."
+	export TRACKING_TRAFFIC_TTS="A flow of tracking traffic has been identified from a device on the network."
+	npm run start
+	cd ../
