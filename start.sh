c87b111dcaac1ae8350aee9f19719ed9d2a79c19
Taufiqur Ashrafy <tashrafy91@gmail.com>
2022-03-26 21:00:29 -0400

    chore(start): adding comments and env variables to configure to start script

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Start dockerized applications:
+#	- MongoDB
+# 	- Iot Inspector Local
+# 	- Iot Inspector Agent
+# 	- Iot Traffic Lights
 export UUID=$(cat /proc/sys/kernel/random/uuid)
 echo $UUID
 docker-compose up -d &
 sleep 45
-echo $UUID
+
+# Start standalone applications:
+# 	- Iot Sound Modality
 cd iot-traffic-sound
+export ENABLE_AUDIO=true
+export WARNING_TRAFFIC_TTS="A flow of potentially concerning traffic has been identified from a device on the network."
+export TRACKING_TRAFFIC_TTS="A flow of tracking traffic has been identified from a device on the network."
 npm run start
 cd ../
